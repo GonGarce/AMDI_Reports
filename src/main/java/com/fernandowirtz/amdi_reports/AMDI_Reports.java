74fe0d9af4e3c30e0a035e0718a732e022a1be3a
GonGarce <gongarce@gmail.com>
2024-12-18 18:15:44 +0100

    Initial commit

---
+++ src/main/java/com/fernandowirtz/amdi_reports/AMDI_Reports.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fernandowirtz.amdi_reports;
+
+/**
+ *
+ * @author gag
+ */
+public class AMDI_Reports {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
